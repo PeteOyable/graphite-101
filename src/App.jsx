7ed42fc0d32d178cba410db9b8b1a877b6428878
PeteOyable <julien.boulevart@gmail.com>
2024-01-11 20:27:44 -0500

    Changes feature a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,25 +6,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Hello world!</h1>
+      <p><strong>This is a super counter. All you have to do is click on the button, and the see the magic happening.</strong></p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+         Count me in
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        The count is {count}
       </p>
     </>
   )
